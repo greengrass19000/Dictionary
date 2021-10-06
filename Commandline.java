c0164a76c3b8e55a44a269481b10924eeb2fcc94
GreenGrass <72069705+greengrass19000@users.noreply.github.com>
2021-10-06 15:36:37 +0700

    Add files via upload

---
+++ Commandline.java
@@ -0,0 +1,25 @@
+public class Commandline {
+    //Check if the word has illegal character
+    public boolean Checkword(String word) {
+        for(char l : word.toCharArray()) {
+            if((l > 'z' || l < 'a') && (l > 'Z' || l < 'A')) {
+                System.out.println("Illegal character!!!");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        TrieDataStructure Dictionary = new TrieDataStructure();
+        Dictionary.insert("dear", "1");
+        Dictionary.insert("deal", "2");
+        Dictionary.insert("do", "3");
+        Dictionary.insert("he", "4");
+        Dictionary.insert("hen", "5");
+        Dictionary.insert("heat", "6");
+
+        Dictionary.change("hee", "99");
+        Dictionary.ShowAllWords(Dictionary.getRoot(), "");
+    }
+}
