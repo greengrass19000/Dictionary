e2e0a890d9b5273dd64b90f862eb63dc6a286e89
0thElement <quangquangtrung89@gmail.com>
2021-10-11 01:24:50 +0700

    CSS styling + fixing warnings

--- src/main/java/com/example/dictionary/DictionaryApplication.java
+++ src/main/java/com/example/dictionary/DictionaryApplication.java
@@ -1,17 +1,21 @@
 package com.example.dictionary;
 
 import javafx.application.Application;
+import javafx.css.Stylesheet;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class DictionaryApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, NullPointerException {
         FXMLLoader fxmlLoader = new FXMLLoader(DictionaryApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        String css = Objects.requireNonNull(DictionaryApplication.class.getResource("style.css")).toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setTitle("Dictionary");
         stage.setScene(scene);
         stage.show();
